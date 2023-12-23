0696bc36c4629229600b1c1d6bd452fe6d6988f0
Natasha England-Elbro <natasha@natashaee.me>
2023-12-23 21:49:18 +0000

    fix: not adding compression header

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,30 @@ use std::{fs::File, io::Write};
 use image::{io::Reader as ImageReader, DynamicImage, EncodableLayout};
 use swizzle_3ds::swizzle_in_place;
 
+enum CompressionType {
+    None,
+}
+
+fn compression_header(ty: CompressionType, data_sz: u32) -> Vec<u8> {
+    let magic = match ty {
+        CompressionType::None => 0x0,
+    };
+    let mut buf = Vec::new();
+    buf.push(magic);
+    let sz_bytes = data_sz.to_le_bytes();
+
+    buf.extend_from_slice(&sz_bytes[0..3]);
+    if sz_bytes[3] != 0 {
+        // special case, it requires 4 bytes, set the marker that we are using it
+        buf[0] |= 0x80;
+        buf.push(sz_bytes[3]);
+        // reserved bytes
+        buf.resize(buf.len() + 3, 0);
+    }
+
+    buf
+}
+
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     let input = &args[1];
@@ -17,5 +41,11 @@ fn main() {
         .pixels()
         .flat_map(|px| px.0.iter().copied().rev())
         .collect::<Vec<_>>();
+    outfile
+        .write_all(&compression_header(
+            CompressionType::None,
+            bytes.len().try_into().expect("couldn't fit bytes into u32"),
+        ))
+        .unwrap();
     outfile.write_all(&bytes).unwrap();
 }
