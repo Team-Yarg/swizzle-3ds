c9e023a00616bc6bb3095481f973f8c7b4fd0000
Natasha England-Elbro <natasha@natashaee.me>
2023-12-23 20:18:02 +0000

    feat: works I think

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,85 @@ const TILE_IDX_REMAP: [usize; 64] = [
     0x24, 0x25, 0x2C, 0x2D, 0x26, 0x27, 0x2E, 0x2F, 0x34, 0x35, 0x3C, 0x3D, 0x36, 0x37, 0x3E, 0x3F,
 ];
 
+struct Image<'d, Px> {
+    data: &'d mut [Px],
+    width: usize,
+}
+
+impl<'d, Px> Image<'d, Px> {
+    fn get_tile(&self, x: usize, y: usize) -> Tile<Px> {
+        let start = self.mk_idx(x, y);
+        let finish = self.mk_idx(x + 7, y + 7);
+        assert!(start < self.len());
+        assert!(finish < self.len());
+
+        let mut buf: [MaybeUninit<Px>; 64] = unsafe { MaybeUninit::uninit().assume_init() };
+
+        for row in 0..8 {
+            unsafe {
+                core::ptr::copy_nonoverlapping(self.get(x, y + row), buf[row * 8].as_mut_ptr(), 8);
+            }
+        }
+        Tile {
+            data: unsafe { core::mem::transmute_copy::<_, [Px; 64]>(&buf) },
+            x_offset: x,
+            y_offset: y,
+        }
+    }
+    fn apply_tile(&mut self, tile: Tile<Px>) {
+        for row in 0..8 {
+            unsafe {
+                core::ptr::copy_nonoverlapping(
+                    &tile.data[row * 8],
+                    self.get_mut(tile.x_offset, tile.y_offset + row),
+                    8,
+                )
+            }
+        }
+    }
+    fn get(&self, x: usize, y: usize) -> &Px {
+        &self.data[self.mk_idx(x, y)]
+    }
+
+    fn get_mut(&mut self, x: usize, y: usize) -> &mut Px {
+        &mut self.data[self.mk_idx(x, y)]
+    }
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+    fn mk_idx(&self, x: usize, y: usize) -> usize {
+        y * self.width + x
+    }
+    pub fn new_from_raw(data: &'d mut [Px], width: usize) -> Self {
+        assert!(data.len().is_power_of_two(), "");
+        assert!(width.is_power_of_two());
+        let height = data.len() / width;
+        assert_eq!(width, height);
+
+        assert_eq!(width % 8, 0);
+        assert_eq!(height % 8, 0);
+        Self { data, width }
+    }
+    pub fn swizzle_in_place(&mut self) {
+        for x in (0..self.width).step_by(8) {
+            for y in (0..self.height()).step_by(8) {
+                let mut tile = self.get_tile(x, y);
+                swizzle_tile(&mut tile.data);
+                self.apply_tile(tile);
+            }
+        }
+    }
+    pub fn height(&self) -> usize {
+        self.len() / self.width
+    }
+}
+
+struct Tile<T> {
+    data: [T; 64],
+    x_offset: usize,
+    y_offset: usize,
+}
+
 /// swizzle an 8x8 tile
 pub fn swizzle_tile<T>(tile: &mut [T]) {
     assert_eq!(tile.len(), 64);
@@ -20,7 +99,7 @@ pub fn swizzle_tile<T>(tile: &mut [T]) {
     }
 }
 
-pub fn swizzle_in_place<T>(img: &mut &[T], width: usize) {
+pub fn swizzle_in_place<T>(img: &mut [T], width: usize) {
     assert!(img.len().is_power_of_two(), "");
     assert!(width.is_power_of_two());
     let height = img.len() / width;
@@ -28,26 +107,14 @@ pub fn swizzle_in_place<T>(img: &mut &[T], width: usize) {
 
     assert_eq!(width % 8, 0);
     assert_eq!(height % 8, 0);
-    let mk_idx = |x, y| y * width + x;
 
-    let w_tiles = width / 8;
-    let h_tiles = height / 8;
+    let mut info = Image { data: img, width };
 
-    for x in 0..w_tiles {
-        for y in 0..h_tiles {
-            unsafe {
-                let img_p = img.as_ptr();
-                let mut tile: [T; 64] = MaybeUninit::uninit().assume_init();
-                let offset_x = x * 8;
-                let offset_y = y * 8;
-                for ypx in 0..8 {
-                    core::ptr::copy_nonoverlapping(
-                        img[mk_idx(offset_x, offset_y + ypx)],
-                        tile.as_mut_ptr().add(mk_idx()),
-                        count,
-                    )
-                }
-            }
+    for x in (0..width).step_by(8) {
+        for y in (0..height).step_by(8) {
+            let mut tile = info.get_tile(x, y);
+            swizzle_tile(&mut tile.data);
+            info.apply_tile(tile);
         }
     }
 }
@@ -57,9 +124,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn swizzle_basic() {
+    fn swizzle_tile_basic() {
         let mut input = (0..64).collect::<Vec<usize>>();
         swizzle_tile(&mut input);
+        assert_eq!(input, TILE_IDX_REMAP);
+    }
+
+    #[test]
+    fn swizzle_basic() {
+        let mut input = (0..64).collect::<Vec<usize>>();
+        swizzle_in_place(&mut input, 8);
         assert_eq!(input, TILE_IDX_REMAP);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let args = std::env::args().collect::<Vec<_>>();
+    let input = &args[1];
+    let output = &args[2];
+}
